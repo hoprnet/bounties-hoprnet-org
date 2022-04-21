1ff5ee89ef71ee6a20c0439bb7a97672c2c803c0
nioni <stivennoni77@gmail.com>
2022-04-21 20:03:57 +0300

    remove ;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="/hopr-favicon.svg" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
