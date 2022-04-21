77740655bb6c9a1748223b16a29504d58450e7fa
nioni <stivennoni77@gmail.com>
2022-04-21 11:27:28 +0300

    remove placeholder video

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -33,7 +33,7 @@ const AboutUsPage: NextPage = () => {
             <iframe
               width="460"
               height="259"
-              src="https://www.youtube.com/embed/DLpF7NQ3CTo"
+              src=""
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
