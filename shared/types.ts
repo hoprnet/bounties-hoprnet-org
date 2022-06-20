00635dca54d6d2ff49a25e28673b4ad8c0b9d2f6
Michal Jadach <michal.jadach@hoprnet.org>
2022-06-20 14:19:43 +0200

    boolean typo fix

--- shared/types.ts
+++ shared/types.ts
@@ -7,7 +7,7 @@ export interface Bounty {
   completedOn: string;
   bountyGithubUrl: string;
   bountyServiceUrl: string;
-  contest: Boolean;
+  contest: boolean;
 }
 
 export interface BountyCompleted extends Bounty {
